4c91155af3f62d1b606a9c889194fc9cf3403b10
Galib Mehboob <86629449+Galibmehboob@users.noreply.github.com>
2026-07-20 23:40:27 +0600

    fix role in auth session

--- src/auth.ts
+++ src/auth.ts
@@ -1,60 +1,67 @@
-import "dotenv/config";
+  import "dotenv/config";
 
-import { betterAuth } from "better-auth";
-import { createAuthMiddleware } from "better-auth/api";
-import { mongodbAdapter } from "@better-auth/mongo-adapter";
-import { MongoClient } from "mongodb";
+  import { betterAuth } from "better-auth";
+  import { createAuthMiddleware } from "better-auth/api";
+  import { mongodbAdapter } from "@better-auth/mongo-adapter";
+  import { MongoClient } from "mongodb";
 
-const client = new MongoClient(process.env.MONGODB_URI!);
+  const client = new MongoClient(process.env.MONGODB_URI!);
 
-await client.connect();
-console.log(" MongoDB Connected Successfully");
+  await client.connect();
+  console.log(" MongoDB Connected Successfully");
 
-export const db = client.db(process.env.DB_NAME!);
-console.log(` Database: ${db.databaseName}`);
-export const auth = betterAuth({
-  database: mongodbAdapter(db),
+  export const db = client.db(process.env.DB_NAME!);
+  console.log(` Database: ${db.databaseName}`);
+  export const auth = betterAuth({
+    database: mongodbAdapter(db),
 
-  secret: process.env.BETTER_AUTH_SECRET!,
+    secret: process.env.BETTER_AUTH_SECRET!,
 
-  baseURL: process.env.BETTER_AUTH_URL!,
+    baseURL: process.env.BETTER_AUTH_URL!,
 
-  trustedOrigins: [process.env.CLIENT_URL!],
+    trustedOrigins: [process.env.CLIENT_URL!],
 
-  emailAndPassword: {
-    enabled: true,
-  },
+    emailAndPassword: {
+      enabled: true,
+    },
 
-  user: {
-    additionalFields: {
-      role: {
-        type: "string",
-        required: false,
-        defaultValue: "customer",
-        input: true,
+    user: {
+      additionalFields: {
+        role: {
+          type: "string",
+          required: false,
+          defaultValue: "customer",
+          input: true,
+        },
       },
     },
+
+    hooks: {
+      before: createAuthMiddleware(async (ctx) => {
+        if (ctx.path !== "/sign-up/email") {
+          return;
+        }
+
+        const body = ctx.body as Record<string, unknown>;
+
+        return {
+          context: {
+            ...body,
+            role: body.role === "seller" ? "seller" : "customer",
+          },
+        };
+      }),
+    },
+
+   session: {
+  expiresIn: 60 * 60 * 24 * 7,
+  updateAge: 60 * 60 * 24,
+
+  additionalFields: {
+    role: {
+      type: "string",
+      required: false,
+    },
   },
-
-  hooks: {
-    before: createAuthMiddleware(async (ctx) => {
-      if (ctx.path !== "/sign-up/email") {
-        return;
-      }
-
-      const body = ctx.body as Record<string, unknown>;
-
-      return {
-        context: {
-          ...body,
-          role: body.role === "seller" ? "seller" : "customer",
-        },
-      };
-    }),
-  },
-
-  session: {
-    expiresIn: 60 * 60 * 24 * 7,
-    updateAge: 60 * 60 * 24,
-  },
-});
+},
+  });
